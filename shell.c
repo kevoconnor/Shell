c934ce27f34e5d18c1812ebe0c6925a95d9686e1
= <kevin_oconnor@taylor.edu>
2017-02-12 22:52:56 -0500

    Initial commit with basic printf statement

---
+++ shell.c
@@ -0,0 +1,20 @@
+/*
+
+Kevin O'Connor
+February 2017
+
+COS421 Operation Systems
+Shell implementation
+
+*/
+
+#include "stdio.h"
+
+int main( int argc, char * argv[ ] )
+{
+
+    printf( "Shell prompt \n" );
+
+    return 0;
+
+}
