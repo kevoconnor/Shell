34a227f9e2f04b6910396d795d9dbde53c2a0cb8
= <kevin_oconnor@taylor.edu>
2017-02-16 16:58:44 -0500

    Added simple string comparisons for cd, pwd, version and exit

--- shell.c
+++ shell.c
@@ -10,6 +10,7 @@ Shell implementation
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 int main( int argc, char * argv[ ] )
 {
@@ -17,27 +18,36 @@ int main( int argc, char * argv[ ] )
     while( 1 )
     {
 
-	char args[ 100 ];
-	
-	printf( "\n%% " );
-	fgets( args, 100, stdin );
-	
-	/*
-	switch( argv[ 1 ] )
+	//char * args;
+	char args[ 10 ];
+
+	printf( "%% " );
+	//fgets( args, 10, stdin );
+	scanf( "%s", args );
+
+	if( strcmp( args, "exit" ) == 0 )
 	{
+	    printf( "Exiting program\n" );
+	    return 0;
+	}
+	else if ( strcmp( args, "cd" ) == 0 )
+	{
+	    printf( "Change directory\n" );
+	}
+	else if ( strcmp( args, "pwd" ) == 0 )
+	{
+	    printf( "Print working directory\n" );
+	}
+	else if( strcmp( args, "version" ) == 0 )
+	{
+	    printf( "+----- Version 0.1.1 -----+\n+---- Kevin O\'Connor -----+\n+---- Revised 2/16/17 ----+\n" );
+	}
+	else
+	{
+	    printf( "Command not found\n" );
+	}
 
-	    // case ls
-
-	    // case pwd
-
-  	    // case cat
-
-	} 
-	printf( "\n>" );
-	*/
     }
-
-    //printf( "Shell prompt \n" );
 
     return 0;
 
